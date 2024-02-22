5b0dba3e776e658e213927fad9b16d654e7a64cd
prashanth <reddyprashanth512@gmail.com>
2024-02-22 04:00:36 +0000

    solution

---
+++ src/components/Home/index.js
@@ -0,0 +1,42 @@
+import './index.css'
+
+const Home = () => (
+  <div className="home-container">
+    <h1 className="games-heading">Games</h1>
+    <div className="card-show-container">
+      <div className="game-card-container">
+        <img
+          src="https://res.cloudinary.com/dlsuy2qn2/image/upload/v1708572208/Asset_1_4x_1_1_vypqbi.png"
+          className="emoji"
+          alt="emoji"
+        />
+        <h1 className="emoji-name">Emoji Game</h1>
+      </div>
+      <div className="game-card-container">
+        <h1 className="memory-name">Memory Matrix</h1>
+        <img
+          src="https://res.cloudinary.com/dlsuy2qn2/image/upload/v1708572317/memory_1_bvfmai.png"
+          className="memory"
+          alt="emoji"
+        />
+      </div>
+      <div className="game-card-container">
+        <h1 className="memory-name">ROCK PAPER SCISSOR</h1>
+        <img
+          src="https://res.cloudinary.com/dlsuy2qn2/image/upload/v1708572334/Group_7469_1_i9ovya.png"
+          className="rock"
+          alt="emoji"
+        />
+      </div>
+      <div className="game-card-container">
+        <h1 className="memory-name">Card-Flip Memory Game</h1>
+        <img
+          src="https://res.cloudinary.com/dlsuy2qn2/image/upload/v1708572327/Layer_2_yiiit6.png"
+          className="flip"
+          alt="flip"
+        />
+      </div>
+    </div>
+  </div>
+)
+export default Home
